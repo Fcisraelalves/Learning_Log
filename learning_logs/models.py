86cf9d1f4465cea7af5646bb4dd8749ffae35d1d
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-01-20 17:22:22 -0300

    Fix: make migrations

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Topic(models.Model):
+    text = models.CharField(max_length=30)
+    date_added = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+    
+class Entry(models.Model):
+    text = models.TextField(max_length=100)
+    date_added = models.DateTimeField(auto_now=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
