51f36660c1f9e3e1636607fd2e1db742e75a624f
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-01-22 09:01:29 -0300

    feat: add a owner for each Topic.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     text = models.CharField(max_length=30)
     date_added = models.DateTimeField(auto_now=True)
-
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
     def __str__(self):
         return self.text
     
